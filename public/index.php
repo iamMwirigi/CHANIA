8e84d1bb01e03be564f9129e33fac9636196ffb9
Mwirigi <mwirigimburugu81@gmail.com>
2025-06-24 12:28:51 +0300

    chania redeploy

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+$dotenv->safeLoad();
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
