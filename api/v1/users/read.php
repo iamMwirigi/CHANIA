332d931f0eb8345cf8edb3c1b8184ff6a9aecd52
Mwirigi <mwirigimburugu81@gmail.com>
2025-06-24 17:49:49 +0300

    users

--- api/v1/users/read.php
+++ api/v1/users/read.php
@@ -3,8 +3,8 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-include_once '../../../config/Database.php';
-include_once '../../../models/User.php';
+include_once __DIR__ . '/../../../config/Database.php';
+include_once __DIR__ . '/../../../models/User.php';
 
 $database = new Database();
 $db = $database->getConnection();
--- api/v1/users/update.php
+++ api/v1/users/update.php
@@ -6,8 +6,8 @@ header("Access-Control-Allow-Methods: PUT");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-include_once '../../../config/Database.php';
-include_once '../../../models/User.php';
+include_once __DIR__ . '/../../../config/Database.php';
+include_once __DIR__ . '/../../../models/User.php';
 
 $database = new Database();
 $db = $database->getConnection();
